da3278db9615aaf5a6f30725f5dd2bd56e066ca1
Kevin de Guzman <105267565+kevde-impakt@users.noreply.github.com>
2022-10-08 17:39:25 +0800

    My Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var count = 0;
+
+var integerZero = 0;
+var stringZero = '0';
+var booleanVariable = false;
+var cat = {
+    feet: 4,
+    eyes: 2,
+    name: 'Mingming',
+};
+
+function showMyHtmlDocument() {
+    document.body.getElementsByTagName('div')[0].innerHTML = "HTML is Shown";
+}
+
+function showMyCat() {
+    var message = `My Cat's name is ${cat.name}`;
+    document.getElementById('cat-name').innerHTML = message;
+}
+
+function isZeroEqual() {
+    const isZero = integerZero == stringZero;
+    alert(`Integer Zero is equals to String Zero: ${isZero}`);
+}
+
+function sayHello() {
+    alert('Hello its my first Javascript code');
+}
+
+function addCount() {
+    count++;
+    document.getElementsByClassName('count')[0].innerHTML = count;
+}
